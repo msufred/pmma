94bc08189c79f94edbf5ac852e43075782a3e9a6
msufred <msufred@gmail.com>
2016-08-09 15:17:58 +0800

    Added NONE and NOT_EDITABLE mode  constants.

--- src/com/gemseeker/pmma/ui/components/FormLabel.java
+++ src/com/gemseeker/pmma/ui/components/FormLabel.java
@@ -1,9 +1,6 @@
 package com.gemseeker.pmma.ui.components;
 
-import javafx.application.Platform;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.shape.SVGPath;
 
 /**
  *
@@ -11,19 +8,25 @@ import javafx.scene.shape.SVGPath;
  */
 public class FormLabel extends Label {
     
+    public static final int NONE = -2;
     public static final int TOO_SHORT = 0;
-    public static final int ALREADY_EXISTS = 1;
-    public static final int ALLOWED = 2;
-    public static final int ERROR = 3;
+    public static final int ALREADY_EXISTS = 2;
+    public static final int ALLOWED = 4;
+    public static final int ERROR = 6;
+    public static final int NOT_EDITABLE = 8;
     
     private int mStyle;
     
     public FormLabel(){
-        setPrefWidth(60);
+        setPrefWidth(100);
     }
     
     public void setGraphic(int style){
         switch(style){
+            // NOTICE that getStyleClass().clear() is always called for each
+            // case. The logic is that this FormLabel changes its state from
+            // time to time. If getStyleClass().clear() is not called, the styles
+            // will stack up and that will make this FormLabel look weird.
             case TOO_SHORT:
                 getStyleClass().clear();
                 getStyleClass().add("form-label-error");
@@ -43,6 +46,11 @@ public class FormLabel extends Label {
                 getStyleClass().clear();
                 getStyleClass().add("form-error");
                 setText("");
+                break;
+            case NOT_EDITABLE:
+                getStyleClass().clear();
+                getStyleClass().add("form-label-error");
+                setText("Not Editable");
                 break;
             default:
                 getStyleClass().clear();
