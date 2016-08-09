69f8b6dd924fa45e6111bfda833366381778cd53
msufred <msufred@gmail.com>
2016-08-09 15:20:59 +0800

    Added String constants for Contractors table and its columns.

--- src/com/gemseeker/pmma/data/DBUtil.java
+++ src/com/gemseeker/pmma/data/DBUtil.java
@@ -7,37 +7,63 @@ package com.gemseeker.pmma.data;
 public final class DBUtil {    
     
     public static final class Tables {
-        static final String PROJECTS = "Projects";
-        static final String HISTORIES = "Histories";
-        static final String LOCATIONS = "Locations";
-        static final String COORDINATES = "Coordinates";
+        public static final String PROJECTS = "Projects";
+        public static final String HISTORIES = "Histories";
+        public static final String LOCATIONS = "Locations";
+        public static final String COORDINATES = "Coordinates";
+        public static final String CONTRACTORS = "Contractors";
+        public static final String EMAILS = "Emails";
+        public static final String PHONE_NUMBERS = "Phone Numbers";
+        public static final String MOBILE_NUMBERS = "Mobile Numbers";
     }
     
     public static final class Columns {
         // PROJECT
-        static final String PROJECT_CODE = "Code";
-        static final String PROJECT_NAME = "Project Name";
-        static final String PROJECT_LOCATION_ID = "Location ID";
-        static final String PROJECT_DATE_STARTED = "Date Started";
-        static final String PROJECT_DATE_TO_FINISH = "Date to Finish";
-        static final String PROJECT_STATUS = "Status";
+        public static final String PROJECT_CODE = "Code";
+        public static final String PROJECT_NAME = "Project Name";
+        public static final String PROJECT_LOCATION_ID = "Location ID";
+        public static final String PROJECT_CONTRACTOR_ID = "Contractor ID";
+        public static final String PROJECT_DATE_STARTED = "Date Started";
+        public static final String PROJECT_DATE_TO_FINISH = "Date to Finish";
+        public static final String PROJECT_STATUS = "Status";
         
         // HISTORY
-        static final String HISTORY_ID = "ID";
-        static final String HISTORY_PROJECT_CODE = "Project Code";
-        static final String HISTORY_DATE_CREATED = "Date Created";
-        static final String HISTORY_NOTES = "Notes";
+        public static final String HISTORY_ID = "ID";
+        public static final String HISTORY_PROJECT_CODE = "Project Code";
+        public static final String HISTORY_DATE_CREATED = "Date Created";
+        public static final String HISTORY_NOTES = "Notes";
         
         // LOCATION
-        static final String LOCATION_ID = "ID";
-        static final String LOCATION_STREET = "Street";
-        static final String LOCATION_CITY = "City";
-        static final String LOCATION_PROVINCE = "Province";
+        public static final String LOCATION_ID = "ID";
+        public static final String LOCATION_STREET = "Street";
+        public static final String LOCATION_CITY = "City";
+        public static final String LOCATION_PROVINCE = "Province";
         
         // CORRDINATE
-        static final String COORDINATE_ID = "ID";
-        static final String COORDINATE_PROJECT_CODE = "Project Code";
-        static final String COORDINATE_LATITUDE = "Latitude";
-        static final String COORDINATE_LONGITUDE = "Longitude";
+        public static final String COORDINATE_ID = "ID";
+        public static final String COORDINATE_PROJECT_CODE = "Project Code";
+        public static final String COORDINATE_LATITUDE = "Latitude";
+        public static final String COORDINATE_LONGITUDE = "Longitude";
+        
+        // CONTRACTORS
+        public static final String CONTRACTOR_ID = "ID";
+        public static final String CONTRACTOR_NAME = "Company Name";
+        public static final String CONTRACTOR_ADDRESS = "Address";
+        public static final String CONTRACTOR_CONTACT_PERSON = "Contact Person";
+        
+        // EMAILS
+        public static final String EMAIL_ID = "ID";
+        public static final String EMAIL_USER_ID = "User ID";
+        public static final String EMAIL_ADDRESS = "Email";
+        
+        // PHONE NUMBERS
+        public static final String PHONE_ID = "ID";
+        public static final String PHONE_USER_ID = "User ID";
+        public static final String PHONE_NUMBER = "Phone";
+        
+        // MOBILE NUMBERS
+        public static final String MOBILE_ID = "ID";
+        public static final String MOBILE_USER_ID = "User ID";
+        public static final String MOBILE_NUMBER = "Mobile";
     }
 }
