568fa5988aec659c9fd5e06ff7296ecc180eaca4
msufred <msufred@gmail.com>
2016-08-09 15:24:21 +0800

    Added constants for ControlledScreen states. ControlledScreen can set its content using an fxml file by invoking setContentView in the constructor. I added methods such as onStart. onResume, onPause and onFinish.

--- src/com/gemseeker/pmma/ControlledScreen.java
+++ src/com/gemseeker/pmma/ControlledScreen.java
@@ -1,8 +1,9 @@
 package com.gemseeker.pmma;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Parent;
+import java.io.IOException;
+import java.net.URL;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 
 /**
  *
@@ -10,8 +11,18 @@ import javafx.scene.Parent;
  */
 public abstract class ControlledScreen {
 
+    public static final int CREATED = 0;
+    public static final int STARTED = 1;
+    public static final int PAUSED = 2;
+    public static final int RESUMED = 3;
+    public static final int FINISHED = 4;
+    
+    protected int mState;
+    
     protected ScreenController screenController;
+    protected Node contentView;
     protected final String name;
+    private boolean isChild = false;
     
     public ControlledScreen(String name){
         /*
@@ -20,6 +31,7 @@ public abstract class ControlledScreen {
         on finish actions specific for every screen
         */
         this.name = name;
+        mState = CREATED;
     }
     
     public void setScreenController(ScreenController controller){
@@ -34,7 +46,52 @@ public abstract class ControlledScreen {
         return name;
     }
     
-    public abstract Parent getContentView();
+    public void setContentView(URL location){
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(location);
+        loader.setController(this);
+        try{
+            loader.load();
+            contentView = loader.getRoot();
+        }catch(IOException e){
+            System.err.println("Failed to load fxml file from " + location.getFile());
+            System.err.println(e);
+        }
+    }
     
-    public abstract EventHandler<ActionEvent> getOnLoadEvent();
+    public final void setContentView(Node content){
+        contentView = content;
+    }
+    
+    public final Node getContentView(){
+        return contentView;
+    }
+    
+    public void onStart(){
+        mState = STARTED;
+    }
+    
+    public void onPause(){
+        mState = PAUSED;
+    }
+    
+    public void onResume(){
+        mState = RESUMED;
+    }
+    
+    public void onFinish(){
+        mState = FINISHED;
+    }
+    
+    public int getState(){
+        return mState;
+    }
+
+    public final void setAsChild(boolean isChild){
+        this.isChild = isChild;
+    }
+    
+    public boolean isChild(){
+        return isChild;
+    }
 }
